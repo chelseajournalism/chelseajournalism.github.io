4c30b817af7cb44b7f70256ff6e81bcdd0b05aaa
chelseajournalism <104483176+chelseajournalism@users.noreply.github.com>
2022-04-26 23:45:19 -0700

    Create jquery.js

---
+++ jquery.js
@@ -0,0 +1,8 @@
+// Copied from: https://stackoverflow.com/a/31837264
+$(function () {
+  var includes = $('[data-include]')
+  $.each(includes, function () {
+    var file = 'views/' + $(this).data('include') + '.html'
+    $(this).load(file)
+  })
+})
